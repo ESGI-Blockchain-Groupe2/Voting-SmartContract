affc28697f976c3b731753dddaf2d3a9e24c7801
alexandrebis <alex.bis8@hotmail.fr>
2021-01-09 15:45:49 +0100

    Add licence and tests

--- test/TestElectionFactory.sol
+++ test/TestElectionFactory.sol
@@ -1,10 +1,9 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
-//import "..//contracts//ElectionFactory.sol";
-import "..\\contracts\\ElectionFactory.sol";
-import "..\contracts\ElectionFactory.sol";
+import "../contracts/ElectionFactory.sol";
 
 contract TestElectionFactory {
     function test_election_creation() public {
@@ -13,10 +12,8 @@ contract TestElectionFactory {
         candidatesNames.push("George H. W. Bush");
         candidatesNames.push("Bill Clinton");
         candidatesNames.push("Ross Perot");
-        Election election = electionFactory._createElection("USA president election", candidatesNames);
-
-        Election expected = Election();
-
+        electionFactory._createElection("USA president election", candidatesNames);
+        electionFactory.seeE    lection(0)
         Assert.equal(election, expected, "Election should be initialized correctly");
     }
 }
